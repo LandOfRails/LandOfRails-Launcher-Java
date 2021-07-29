39e199f675c80a5179a8f979e83e69acfd6188b8
Danielxs01 <daniel.schmidtdsi@gmail.com>
2021-07-30 01:34:28 +0200

    # Added elements like textfields and other to login.fxml # Added Controller Login.java

--- src/main/java/net/landofrails/Login.java
+++ src/main/java/net/landofrails/Login.java
@@ -1,4 +1,32 @@
 package net.landofrails;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import net.landofrails.minecraft.Minecraft;
+
 public class Login {
+
+    @FXML
+    private TextField email;
+
+    @FXML
+    private PasswordField password;
+
+    @FXML
+    private CheckBox remember;
+
+    @FXML
+    private Label inputwasincorrect;
+
+    @FXML
+    protected void handleLoginButton(ActionEvent event) {
+        boolean correct = Minecraft.getInstance().login(email.getText(), password.getText());
+        if (!correct)
+            inputwasincorrect.setVisible(true);
+    }
+
 }
